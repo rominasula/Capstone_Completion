6507b2cca3890032674590cd1235e224cbf070c3
rominasula <romina_sula@hotmail.com>
2025-11-18 20:17:42 -0500

    creating routes

---
+++ backend/routes/projects.js
@@ -0,0 +1,14 @@
+import express from "express";
+import Project from "../models/Project.js";
+
+const router = express.Router();
+
+// Get all projects
+router.get("/", async (req, res) => {
+  const projects = await Project.find();
+  res.json(projects);
+});
+
+
+
+export default router;
