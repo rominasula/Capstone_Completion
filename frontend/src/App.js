3f8c6a82084936e3633a345df0900a6ed52871ea
rominasula <romina_sula@hotmail.com>
2025-11-20 19:38:22 -0500

    fixing app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { ProjectProvider } from "./context/ProjectContext";
 
-
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Projects from "./pages/Projects";
+import ProjectDetails from "./pages/ProjectDetails";
+import TaskForm from "./pages/TaskForm";
 import Navbar from "./components/Navbar";
 
 import "./styles.css";
